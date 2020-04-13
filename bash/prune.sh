21e175d02a4790ffab262a6bb92cf38bcfb39484
Lu Nguyen <lu@redweb.dk>
2020-04-13 15:35:42 +0700

    Slow

--- bash/prune.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#Start docker compose
-echo "- Docker prune..."
-docker -y system prune -a
-docker -y volume prune
--- bash/restore.sh
+++ bash/restore.sh
@@ -5,10 +5,10 @@ dbname=$1
 sitename=$2
 path=$3
 
-docker exec db mysql -uroot -proot -e "DROP DATABASE ${dbname}"
-docker exec db mysql -uroot -proot -e "CREATE DATABASE ${dbname}"
+docker exec mysql mysql -uroot -proot -e "DROP DATABASE ${dbname}"
+docker exec mysql mysql -uroot -proot -e "CREATE DATABASE ${dbname}"
 
-docker exec -i db mysql -uroot -proot --default-character-set=utf8 ${dbname} < ${path}/${sitename}_structure.sql
-docker exec -i db mysql -uroot -proot --default-character-set=utf8 ${dbname} < ${path}/${sitename}_data.sql
+docker exec -i mysql mysql -uroot -proot --default-character-set=utf8 ${dbname} < ${path}/${sitename}_structure.sql
+docker exec -i mysql mysql -uroot -proot --default-character-set=utf8 ${dbname} < ${path}/${sitename}_data.sql
 
 echo "Done."
