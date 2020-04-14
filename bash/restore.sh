95ee830f7c6dbbf72710a8e66a34be259925c019
Lu Nguyen <lu@redweb.dk>
2020-04-14 16:34:44 +0700

    fix

--- bash/restore.sh
+++ bash/restore.sh
@@ -2,13 +2,12 @@
 echo "Restoring database..."
 
 dbname=$1
-sitename=$2
-path=$3
+path=$2
 
 docker exec mysql mysql -uroot -proot -e "DROP DATABASE ${dbname}"
 docker exec mysql mysql -uroot -proot -e "CREATE DATABASE ${dbname}"
 
-docker exec -i mysql mysql -uroot -proot --default-character-set=utf8 ${dbname} < ${path}/${sitename}_structure.sql
-docker exec -i mysql mysql -uroot -proot --default-character-set=utf8 ${dbname} < ${path}/${sitename}_data.sql
+docker exec -i mysql mysql -uroot -proot --default-character-set=utf8 ${dbname} < ${path}_structure.sql
+docker exec -i mysql mysql -uroot -proot --default-character-set=utf8 ${dbname} < ${path}_data.sql
 
 echo "Done."
