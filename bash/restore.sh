363a5c1077723359d0be21240f6d0c14abc5ff6a
Lu Nguyen <lu@Lus-MacBook-Pro.local>
2021-06-22 15:14:42 +0700

    Docker Sync

--- bash/restore.sh
+++ bash/restore.sh
@@ -7,6 +7,9 @@ path=$2
 docker exec mysql mysql -uroot -proot -e "DROP DATABASE ${dbname}"
 docker exec mysql mysql -uroot -proot -e "CREATE DATABASE ${dbname}"
 
+docker exec -i mysql mysql -uroot -proot -e "CREATE USER '${dbname}'@'%' IDENTIFIED BY '${dbname}'"
+docker exec -i mysql mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON ${dbname}.* TO '${dbname}'@'%'"
+
 docker exec -i mysql mysql -uroot -proot --default-character-set=utf8 ${dbname} < ${path}_structure.sql
 docker exec -i mysql mysql -uroot -proot --default-character-set=utf8 ${dbname} < ${path}_data.sql
 
